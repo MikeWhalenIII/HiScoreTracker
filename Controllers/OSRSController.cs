8f67dc3c9504fd00891e250e179e3c97c58f5dd9
MikeWhalenIII <michael1327@hotmail.com>
2020-12-20 11:32:40 -0500

    updates

---
+++ Controllers/OSRSController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiscoreTracker.Controllers
+{
+    public class OSRSController : Controller
+    {
+        public string Index()
+        {
+            return "hello.";
+        }
+
+        public IActionResult Track()
+        {
+            return View();
+        }
+    }
+}
